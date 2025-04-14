4a9ee925061bdae66f7c96e3e27170823389699d
saranyu1990 <87749758+saranyu1990@users.noreply.github.com>
2025-04-14 22:42:47 +0700

    feat(exceptions): add HtmlStructureException and HttpRequestException

---
+++ src/Exceptions/HtmlStructureException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoldPrice\Exceptions;
+
+use Exception;
+
+/**
+ * Class HtmlStructureException
+ *
+ * Thrown when the expected HTML structure is not found during scraping.
+ */
+class HtmlStructureException extends Exception
+{
+}
---
+++ src/Exceptions/HttpRequestException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoldPrice\Exceptions;
+
+use Exception;
+
+/**
+ * Class HttpRequestException
+ *
+ * Thrown when an HTTP request fails or encounters an error.
+ */
+class HttpRequestException extends Exception
+{
+}
